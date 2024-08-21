2f61b2f45052a90c25a572f69bd23a65c70f4f6c
Radiah <radiaaahhh@gmail.com>
2024-08-21 10:46:52 +0100

    Add homepage skeleton

---
+++ src/components/CatList/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {useCat} from "../contexts"
+
+export default function CatList({ratingOrder, breedOnly, setRatingOrder, setBreedOnly}) {
+  const {catData} = useCat();
+
+  const filteredData = catData
+    .filter((cat) => !breedOnly || cat.breed)
+
+  return (
+    <div>
+      <button onClick={() => setBreedOnly(!breedOnly)}>Breeds Only</button>
+      <ul>
+        {filteredData.map((cat) => (
+          <li key={cat.id}>
+            {cat.name} - {cat.breed_group} ({cat.origin})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom";
+import SearchWidget from "./SearchWidget";
 
-export default function HomePage() {
+export default HomePage = () => {
+
+
   return (
-    <div>index</div>
-  )
-}
+    <main>
+      <h1>CatTree</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <SearchWidget />
+    </main>
+  );
+};
