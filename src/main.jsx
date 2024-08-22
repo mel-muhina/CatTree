5b9a2910fab96b7eed18e23fcff82961ec83a851
Mel <160260061+mel-muhina@users.noreply.github.com>
2024-08-22 09:50:00 +0100

    Resolve double loading images.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { CatProvider } from './contexts/index.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CatProvider } from "./contexts/index.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <CatProvider>
-        <App />
-      </CatProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <CatProvider>
+      <App />
+    </CatProvider>
+  </BrowserRouter>
 );
